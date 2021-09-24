6089c89435f566dc7fd0741b70ab906fb38d7ae1
Stévillis Sousa <stevillis@hotmail.com>
2021-09-24 01:42:27 -0400

    Create BaseModel to be the superclass of CRUD Models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(_('Criado em'), auto_now_add=True)
+    updated_at = models.DateTimeField(_('Alterado em'), auto_now=True)
+    created_by = models.ForeignKey(User, related_name="%(app_label)s_%(class)s_related_created_by", null=True,
+                                   blank=True, default=None, on_delete=models.CASCADE)
+    updated_by = models.ForeignKey(User, related_name="%(app_label)s_%(class)s_related_updated_by", null=True,
+                                   blank=True, default=None, on_delete=models.CASCADE)
+    active = models.BooleanField(_('Ativo'), default=True)
+    deactivated_at = models.DateTimeField(_('Inativado em'), null=True, blank=True)
+
+    class Meta:
+        abstract = True
