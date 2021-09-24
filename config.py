c4e5a806aaaf457b4cc2abab678a921a1595860a
Stévillis Sousa <stevillis@hotmail.com>
2021-09-24 01:32:39 -0400

    Change config reader to read non-alphanumeric characters in SECRET_KEY

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import configparser
 
 
 def get_config(section, key):
-    config = configparser.ConfigParser()
+    config = configparser.ConfigParser(interpolation=None)
     config.read('config.ini')
     return config[section][key]
